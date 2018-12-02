0cf7557af65d6745df797dfc4df0b0e5585beb90
Justin Heyes-Jones <justinhj@gmail.com>
2018-12-02 00:03:09 -0800

    Day 2

---
+++ day2/day2.rb
@@ -0,0 +1,38 @@
+# Day 2 of Advent of Code 2018
+# https://adventofcode.com/2018/day/2
+
+# require 'pry-byebug'
+
+filename = ARGV.first || __dir__ + 'input.txt'
+
+file = File.open(filename, 'rb')
+contents = file.read
+lines = contents.split("\n")
+
+# For each letter in a string return a map of letter to freq
+# Returns a tuple of Boolean, the first tells you if the
+# string has two of any char and the second three of any char
+def check_twos_and_threes(str)
+  sa = str.scan(/\w/)
+
+  groups = sa.group_by { |c| c }.values
+
+  has_two = groups.any? { |g| g.length == 2 }
+  has_three = groups.any? { |g| g.length == 3 }
+
+  return has_two ? 1 : 0, has_three ? 1 : 0
+end
+
+counts = lines.reduce([0, 0]) do |acc, line|
+  twos, threes = check_twos_and_threes(line)
+
+  acc[0] = acc[0] + twos
+  acc[1] = acc[1] + threes
+
+  acc
+end
+
+puts counts[0] * counts[1]
+
+
+
---
+++ day2/day2part2.rb
@@ -0,0 +1,65 @@
+# Day 2 of Advent of Code 2018
+# https://adventofcode.com/2018/day/2
+
+require 'pry-byebug'
+
+filename = ARGV.first || __dir__ + 'input.txt'
+
+file = File.open(filename, 'rb')
+contents = file.read
+lines = contents.split("\n")
+
+def remove_diff(str1, str2)
+  a1 = str1.scan(/\w/)
+  a2 = str2.scan(/\w/)
+
+  diffs = a1.zip(a2).reduce("") do |acc, ab|
+
+    a,b = ab
+    
+    if a == b
+      acc + a
+    else
+      acc
+    end
+    
+  end
+
+  
+end
+
+# binding.pry
+
+# we need to find two lines that differ by only one character
+# and return the common characters
+
+diffs = ""
+
+lines.each do |line1|
+
+  lines.each do |line2|
+
+    diffs_removed = remove_diff(line1, line2)
+
+    if diffs_removed.length == (line1.length - 1)
+      diffs = diffs_removed
+  #    binding.pry
+      break diffs
+    end
+  end
+
+  if diffs.length > 0
+    break diffs
+  end
+  
+end
+
+puts diffs
+
+    
+
+    
+
+
+
+
