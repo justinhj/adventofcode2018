e5f12e4dc8d7b051988e4c057d949aa9e0ee0a8e
Justin Heyes-Jones <justinhj@gmail.com>
2018-12-08 10:20:17 -0800

    rubocopy changes

--- day8ruby/day8part1.rb
+++ day8ruby/day8part1.rb
@@ -1,16 +1,15 @@
 require 'set'
 
-require 'pry-byebug'
+# require 'pry-byebug'
 
 filename = ARGV.first || __dir__ + '/input.txt'
 
 file = File.open(filename, 'rb')
 contents = file.read
 
-nums = contents.split(" ").map { |n| n.to_i }
+nums = contents.split(' ').map(&:to_i)
 
-# Parse to a tree
-
+# Parse to a tree, format:
 # node = [num children] [num meta] [nodes] [meta]
 
 class Node
@@ -22,88 +21,66 @@ class Node
 end
 
 # return a class how many array consumed
-def parseTree(start, nums)
-
+def parse_tree(start, nums)
   num_children = nums[start]
-  num_meta = nums[start+1]
+  num_meta = nums[start + 1]
 
   children = []
-  pos = start+2
+  pos = start + 2
 
-#  binding.pry
-  
   unless num_children.zero?
-    (0...num_children).each do |n|
-
-      child, pos = parseTree(pos, nums)
+    (0...num_children).each do |_|
+      child, pos = parse_tree(pos, nums)
       children << child
-    
     end
   end
 
   unless num_meta.zero?
     meta = []
-    (0...num_meta).each do |n|
-      
+    (0...num_meta).each do |_|
+
       meta << nums[pos]
       pos += 1
-      
     end
   end
 
-  return Node.new(children, meta), pos
-  
+  [Node.new(children, meta), pos]
 end
 
-#binding.pry
-
-root, final_pos = parseTree(0, nums)
+root, _ = parse_tree(0, nums)
 
 def sum(tree)
-
   sum = tree.meta.sum
 
   tree.children.each do |n|
 
     sum += sum(n)
-    
   end
 
   sum
-  
 end
 
 puts sum(root)
 
 def sum2(tree)
-
-  #binding.pry
-  
   if tree.children.size.zero?
     sum = tree.meta.sum
-    print "sum meta #{sum}\n"
+    #print "sum meta #{sum}\n"
   else
-
-#    binding.pry
     sum = 0
     tree.meta.each do |n|
 
       n -= 1
-      
-      if n < tree.children.size
 
-        sum += sum2(tree.children[n])
-        print "sum child #{sum}\n"
-        
-      end
-      
+      next unless n < tree.children.size
+
+      sum += sum2(tree.children[n])
+      #print "sum child #{sum}\n"
     end
-
   end
-  print "sum total #{sum}\n"
+  #print "sum total #{sum}\n"
 
   sum
 end
 
 puts sum2(root)
-
