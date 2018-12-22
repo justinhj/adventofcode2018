762c54c2c2b68a86c83b51fd1bd0738e6e1e92ee
Justin Heyes-Jones <justinhj@gmail.com>
2018-12-22 11:28:14 -0800

    done?

--- day13ruby/day13.rb
+++ day13ruby/day13.rb
@@ -1,4 +1,5 @@
 require 'pry-byebug'
+require 'io/console'
 
 filename = ARGV.first || __dir__ + '/input.txt'
 
@@ -22,6 +23,7 @@ def draw_map(m)
   print "\n"
 end
 
+# Parse the map data
 lines.each_with_index do |line, index|
   row = Array.new($width)
   x = 0
@@ -32,7 +34,37 @@ lines.each_with_index do |line, index|
   map[index] = row
 end
 
+# Draw the initial map with cars
 draw_map(map)
+
+def turn_direction(direction, turn)
+  case turn
+  when :left
+    case direction
+    when :up
+      :left
+    when :down
+      :right
+    when :left
+      :down
+    when :right
+      :up
+    end
+  when :straight
+    direction
+  when :right
+    case direction
+    when :up
+      :right
+    when :down
+      :left
+    when :left
+      :up
+    when :right
+      :down
+    end
+  end
+end
 
 def next_turn(current)
   case current
@@ -49,12 +81,25 @@ end
 
 class Car
   attr_accessor :x,:y,:direction,:next_turn
-
+  
   def initialize(x,y,direction)
     @x = x
     @y = y
     @direction = direction
     @next_turn = :left
+  end
+end
+
+def car_direction_to_s(direction)
+  case direction
+  when :down
+    'v'
+  when :up
+    '^'
+  when :right
+    '>'
+  when :left
+    '<'
   end
 end
 
@@ -130,15 +175,92 @@ def draw_cars(cars)
     # Move up
     printf("\u001b[%dA", $height - car.y + 1)
     # Move right
-    printf("\u001b[%dC", car.x)
+    printf("\u001b[%dC", car.x) unless car.x == 0
     
-    printf car.direction + $move_left
+    printf car_direction_to_s(car.direction) + $move_left
 
     # Move down
     printf("\u001b[%dB", $height - car.y + 1)
     # Move left
-    printf("\u001b[%dD", car.x)
+    printf("\u001b[%dD", car.x) unless car.x == 0
   end
+end
+
+def check_collide(positions, x, y)
+
+  if positions.include? [x,y]
+    [positions, true]
+  else
+    positions[[x,y]]=true
+    [positions, false]
+  end
+end
+
+def move_cars(cars, map)
+
+  positions = {}
+  
+  cars = sort_cars(cars)
+  cars.map do |car|
+    m = map[car.y][car.x]
+    if m == '-' and car.direction == :right
+      car.x = car.x + 1
+    elsif m == '-' and car.direction == :left
+      car.x = car.x - 1
+    elsif m == '|' and car.direction == :up
+      car.y = car.y - 1
+    elsif m == '|' and car.direction == :down
+      car.y = car.y + 1
+    elsif m == '/' and car.direction == :up
+      car.x = car.x + 1
+      car.direction = :right
+    elsif m == '/' and car.direction == :left
+      car.y = car.y + 1
+      car.direction = :down
+    elsif m == '/' and car.direction == :down
+      car.x = car.x - 1
+      car.direction = :left
+    elsif m == '/' and car.direction == :right
+      car.y = car.y - 1
+      car.direction = :up
+    elsif m == '\\' and car.direction == :right
+      car.y = car.y + 1
+      car.direction = :down
+    elsif m == '\\' and car.direction == :up
+      car.x = car.x - 1
+      car.direction = :left
+    elsif m == '\\' and car.direction == :left
+      car.y = car.y - 1
+      car.direction = :up
+    elsif m == '\\' and car.direction == :down
+      car.x = car.x + 1
+      car.direction = :right
+    elsif m == '+'
+      car.direction = turn_direction(car.direction, car.next_turn)
+      car.next_turn = next_turn(car.next_turn)
+      case car.direction
+      when :up
+        car.y = car.y - 1
+      when :down
+        car.y = car.y + 1
+      when :left
+        car.x = car.x - 1
+      when :right
+        car.x = car.x + 1
+      end
+    end
+    positions, collide = check_collide(positions, car.x, car.y)
+    if collide
+      print "collide! "
+      pp car
+      k = STDIN.getch
+      if k == 'q'
+        exit
+      end
+
+    end
+  end
+  cars
 end
 
 draw_map(map)
@@ -146,4 +268,12 @@ draw_map(map)
 cars = sort_cars(cars)
 draw_cars(cars)
 
-
+loop do
+  print $move_up * ($height + 1)
+  draw_map(map)
+  print $move_left * 1000
+  cars = move_cars(cars, map)
+  draw_cars(cars)
+  
+#  sleep 0.5
+end
