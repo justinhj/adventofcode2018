6e2f0b7bde6f93a07167f66f93692998b71bf665
Justin Heyes-Jones <justinhj@gmail.com>
2019-01-03 21:29:29 -0800

    tidy up day 14

--- day14ruby/day14part2.rb
+++ day14ruby/day14part2.rb
@@ -63,40 +63,40 @@ def solve(seq)
 
 end
 
-print "\n"
-solve([5,1,5,8,9])
-$recipes = [3,7]
-$elf1 = 0
-$elf2 = 1
+# print "\n"
+# solve([5,1,5,8,9])
+# $recipes = [3,7]
+# $elf1 = 0
+# $elf2 = 1
 
-print "\n"
-solve([0,1,2,4,5])
-$recipes = [3,7]
-$elf1 = 0
-$elf2 = 1
+# print "\n"
+# solve([0,1,2,4,5])
+# $recipes = [3,7]
+# $elf1 = 0
+# $elf2 = 1
 
-print "solving 1 8 7 2 5 1 should be 187251\n" 
-solve([1,8,7,2,5,1])
-$recipes = [3,7]
-$elf1 = 0
-$elf2 = 1
+# print "solving 1 8 7 2 5 1 should be 187251\n" 
+# solve([1,8,7,2,5,1])
+# $recipes = [3,7]
+# $elf1 = 0
+# $elf2 = 1
 
-print "\n"
-solve([9,2,5,1,0])
-$recipes = [3,7]
-$elf1 = 0
-$elf2 = 1
+# print "\n"
+# solve([9,2,5,1,0])
+# $recipes = [3,7]
+# $elf1 = 0
+# $elf2 = 1
 
-print "\n"
-solve([5,9,4,1,4])
+# print "\n"
+# solve([5,9,4,1,4])
 
-print "solving 147061\n"
+# print "solving 147061\n"
 
-$recipes = [3,7]
-$elf1 = 0
-$elf2 = 1
+# $recipes = [3,7]
+# $elf1 = 0
+# $elf2 = 1
 
-solve([1,4,7,0,6,1])
+# solve([1,4,7,0,6,1])
 
 print "635041\n"
 
@@ -107,4 +107,3 @@ $elf2 = 1
 solve([6,3,5,0,4,1])
 
 print "\n"
-
