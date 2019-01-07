e22c879602e26c2671d2188f60756fd6dadfab67
Justin Heyes-Jones <justinhj@gmail.com>
2019-01-06 18:06:49 -0800

    day15 get started

---
+++ day15ruby/part1.rb
@@ -0,0 +1,25 @@
+require 'pry-byebug'
+require 'io/console'
+
+filename = ARGV.first || __dir__ + '/input.txt'
+
+file = File.open(filename, 'rb')
+contents = file.read
+
+lines = contents.split("\n")
+
+map = lines do |line|
+  
+end
+
+# Combat
+# Each unit not dead
+#   resolve all actions
+# Decide who does what based on row then column
+#   try to move in range
+#   attack
+#   if multiple targets priority is top to bottom then left to right
+#    no diagonal attack
+# step 1
+#    identify targets
+
