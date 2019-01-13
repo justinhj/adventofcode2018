b694909ba0be69db1a67e0449ad96e956216596c
Justin Heyes-Jones <justinhj@gmail.com>
2019-01-12 22:25:30 -0800

    part1 done

--- day16go/part1.go
+++ day16go/part1.go
@@ -20,6 +20,16 @@ type Instruction struct {
 	c      int
 }
 
+type Operation struct {
+	opCode  int
+	name    string
+	Execute func(d Device, i Instruction) Device
+}
+
+// type Op interface {
+// 	Execute(d Device, i Instruction) Device
+// }
+
 // Represents a state change we can use
 // to determine what the opcode does
 type Evidence struct {
@@ -101,8 +111,18 @@ func parseEvidence(lines []string, start int) ([]Evidence, int) {
 
 		start += 4
 	}
-
 	return ev, start
+}
+
+func countCandidates(ev Evidence, ops []Operation) int {
+	count := 0
+	for _, op := range ops {
+		result := op.Execute(ev.before, ev.instruction)
+		if result == ev.after {
+			count += 1
+		}
+	}
+	return count
 }
 
 func readLines(path string) ([]string, error) {
@@ -127,12 +147,181 @@ func main() {
 		filename = os.Args[1]
 	}
 
+	ops := []Operation{
+		Operation{opCode: -1,
+			name: "addr",
+			Execute: func(d Device, i Instruction) Device {
+				a := d.registers[i.a]
+				b := d.registers[i.b]
+				d.registers[i.c] = a + b
+				return d
+			},
+		},
+		Operation{opCode: -1,
+			name: "addi",
+			Execute: func(d Device, i Instruction) Device {
+				a := d.registers[i.a]
+				d.registers[i.c] = a + i.b
+				return d
+			},
+		},
+		Operation{opCode: -1,
+			name: "mulr",
+			Execute: func(d Device, i Instruction) Device {
+				a := d.registers[i.a]
+				b := d.registers[i.b]
+				d.registers[i.c] = a * b
+				return d
+			},
+		},
+		Operation{opCode: -1,
+			name: "muli",
+			Execute: func(d Device, i Instruction) Device {
+				a := d.registers[i.a]
+				d.registers[i.c] = a * i.b
+				return d
+			},
+		},
+		Operation{opCode: -1,
+			name: "banr",
+			Execute: func(d Device, i Instruction) Device {
+				a := d.registers[i.a]
+				b := d.registers[i.b]
+				d.registers[i.c] = a & b
+				return d
+			},
+		},
+		Operation{opCode: -1,
+			name: "bani",
+			Execute: func(d Device, i Instruction) Device {
+				a := d.registers[i.a]
+				d.registers[i.c] = a & i.b
+				return d
+			},
+		},
+		Operation{opCode: -1,
+			name: "borr",
+			Execute: func(d Device, i Instruction) Device {
+				a := d.registers[i.a]
+				b := d.registers[i.b]
+				d.registers[i.c] = a | b
+				return d
+			},
+		},
+		Operation{opCode: -1,
+			name: "bori",
+			Execute: func(d Device, i Instruction) Device {
+				a := d.registers[i.a]
+				d.registers[i.c] = a | i.b
+				return d
+			},
+		},
+		Operation{opCode: -1,
+			name: "setr",
+			Execute: func(d Device, i Instruction) Device {
+				a := d.registers[i.a]
+				d.registers[i.c] = a
+				return d
+			},
+		},
+		Operation{opCode: -1,
+			name: "seti",
+			Execute: func(d Device, i Instruction) Device {
+				d.registers[i.c] = i.a
+				return d
+			},
+		},
+		Operation{opCode: -1,
+			name: "gtir",
+			Execute: func(d Device, i Instruction) Device {
+				b := d.registers[i.b]
+				if i.a > b {
+					d.registers[i.c] = 1
+				} else {
+					d.registers[i.c] = 0
+				}
+				return d
+			},
+		},
+		Operation{opCode: -1,
+			name: "gtri",
+			Execute: func(d Device, i Instruction) Device {
+				a := d.registers[i.a]
+				if a > i.b {
+					d.registers[i.c] = 1
+				} else {
+					d.registers[i.c] = 0
+				}
+				return d
+			},
+		},
+		Operation{opCode: -1,
+			name: "gtrr",
+			Execute: func(d Device, i Instruction) Device {
+				a := d.registers[i.a]
+				b := d.registers[i.b]
+				if a > b {
+					d.registers[i.c] = 1
+				} else {
+					d.registers[i.c] = 0
+				}
+				return d
+			},
+		},
+		Operation{opCode: -1,
+			name: "eqir",
+			Execute: func(d Device, i Instruction) Device {
+				b := d.registers[i.b]
+				if i.a == b {
+					d.registers[i.c] = 1
+				} else {
+					d.registers[i.c] = 0
+				}
+				return d
+			},
+		},
+		Operation{opCode: -1,
+			name: "eqri",
+			Execute: func(d Device, i Instruction) Device {
+				a := d.registers[i.a]
+				if a == i.b {
+					d.registers[i.c] = 1
+				} else {
+					d.registers[i.c] = 0
+				}
+				return d
+			},
+		},
+		Operation{opCode: -1,
+			name: "eqrr",
+			Execute: func(d Device, i Instruction) Device {
+				a := d.registers[i.a]
+				b := d.registers[i.b]
+				if a == b {
+					d.registers[i.c] = 1
+				} else {
+					d.registers[i.c] = 0
+				}
+				return d
+			},
+		},
+	}
+
 	lines, error := readLines(filename)
 
 	if error == nil {
-		evidence, next_line := parseEvidence(lines, 0)
+		evidence, _ := parseEvidence(lines, 0)
 
-		fmt.Printf("Success %v\nstart %v", evidence, next_line)
+		answer := 0
+		for _, ev := range evidence {
+			count := countCandidates(ev, ops)
+			fmt.Printf("%d\n", count)
+			if count >= 3 {
+				answer += 1
+			}
+		}
+
+		fmt.Printf("Answer %d\n", answer)
 	} else {
 		fmt.Printf("Error %v\n", error)
 	}
