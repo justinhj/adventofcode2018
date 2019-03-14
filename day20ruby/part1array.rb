cf1a87436b53c79841993df0cb98f47479b6c033
Justin Heyes-Jones <justinhj@gmail.com>
2019-03-13 22:02:56 -0700

    A bunch of day 20 work

--- day20ruby/part1array.rb
+++ day20ruby/part1array.rb
@@ -1,7 +1,6 @@
 # coding: utf-8
 require 'pry-byebug'
 require 'io/console'
-require 'json'
 
 filename = ARGV.first || __dir__ + '/input.txt'
 
@@ -9,12 +8,23 @@ file = File.open(filename, 'rb')
 contents = file.read
 directions = contents.chars
 
+# Map is a 2d array of rooms
+# Room has left right up and down booleans which default to nil
+# We must use array offsets when accessing the map to handle negative x,y
+
+MAP_WIDTH=ARGV[1].to_i || 50
+MAP_HEIGHT=ARGV[2].to_i || 50
+
+ROW_OFFSET=MAP_HEIGHT/2
+COL_OFFSET=MAP_WIDTH/2
+
 Room = Struct.new(:left, :right, :up, :down) do
   def room_count
     0 + (left ? 1 : 0) + (right ? 1 : 0) + (down ? 1 : 0) + (up ? 1 : 0)
   end
-  
 end
+
+rooms = Array.new(MAP_HEIGHT) { Array.new(MAP_WIDTH) { Room.new } }
 
 def draw_room(room)
 
@@ -49,53 +59,41 @@ def draw_room(room)
     s = '<'
   elsif room[:right]
     s = '>'
+  else
+    s = '.'
   end
 
   print s
 
 end
 
-def draw_map(rooms)
+$draws = 0
 
-  puts "\e[H\e[2J"
+def draw_map(rooms, force=false)
+
+  $draws += 1
+
+  return unless $draws % 10000000000 == 0 || force
   
-  min_x = 0
-  max_x = 0
-  min_y = 0
-  max_y = 0
+  # Clear the terminal
+  puts "\e[H\e[2J"
 
-  rooms.each do |coord, room|
-    x,y = coord
-
-    min_x = [x, min_x].min
-    max_x = [x, max_x].max
-
-    min_y = [y, min_y].min
-    max_y = [y, max_y].max
-  end
-
-#  printf "Map extents [#{min_x},#{min_y}] to [#{max_x},#{max_y}]\n\n"
-
-  (min_y..max_y).to_a.reverse.each do |y|
-    (min_x..max_x).to_a.each do |x|
-      room = rooms.key?([x,y])
-      if room
-        draw_room(rooms[[x,y]])
-      else
-        printf ' '
-      end
+  (0...MAP_HEIGHT).each do |row|
+    (0...MAP_WIDTH).each do |col|
+      room = rooms[col][row]
+      draw_room(room)
     end
     print "\n"
   end
-  print "\n"
+  print "\nDraws #{$draws}\n"
+  
+  # k = STDIN.getch
 
-#  k = STDIN.getch
-    
- # if k == 'q'
-  #  exit
-  #end
+  # if k == 'q'
+  #   exit
+  # end
 
-  sleep 0.4
+  sleep 0.2
   
 end
 
@@ -139,71 +137,61 @@ end
 # printf "#{split_routes("EE|(WW|EE)|NN)N$".chars)}\n"
 
 # returns the remaining input
-def expand_map(input, loc, room, rooms, draw)
+def expand_map(input, loc, room, rooms, draw, depth)
   next_move, *remaining = input
+
   loop do
     case next_move
     when '('
       routes = split_routes(remaining)
-      routes.each {|route| expand_map(route, loc, room, rooms, draw)}
+      routes.each {|route| expand_map(route, loc, room, rooms, draw, depth + 1)}
       break
     when '$'
       break
     when 'N'
-      new_loc = [loc[0],loc[1] + 1]
-      new_room = rooms[new_loc]
-      new_room.down = room
-      room.up = new_room
-      rooms[loc] = room
-      rooms[new_loc] = new_room
+      new_loc = [loc[0],loc[1] - 1]
+      new_room = rooms[new_loc[0]][new_loc[1]]
+      new_room.down = true
+      room.up = true
       loc = new_loc
       room = new_room
       draw_map(rooms) if draw
     when 'W'
       new_loc = [loc[0] - 1,loc[1]]
-      new_room = rooms[new_loc]
-      new_room.right = room
-      room.left = new_room
-      rooms[loc] = room
-      rooms[new_loc] = new_room
+      new_room = rooms[new_loc[0]][new_loc[1]]
+      new_room.right = true
+      room.left = true
       loc = new_loc
       room = new_room
       draw_map(rooms) if draw
     when 'S'
-      new_loc = [loc[0],loc[1] - 1]
-      new_room = rooms[new_loc]
-      new_room.up = room
-      room.down = new_room
-      rooms[loc] = room
-      rooms[new_loc] = new_room
+      new_loc = [loc[0],loc[1] + 1]
+      new_room = rooms[new_loc[0]][new_loc[1]]
+      new_room.up = true
+      room.down = true
       loc = new_loc
       room = new_room
       draw_map(rooms) if draw
     when 'E'
       new_loc = [loc[0] + 1,loc[1]]
-      new_room = rooms[new_loc]
-      new_room.left = room
-      room.right = new_room
-      rooms[loc] = room
-      rooms[new_loc] = new_room
+      new_room = rooms[new_loc[0]][new_loc[1]]
+      new_room.left = true
+      room.right = true
       loc = new_loc
       room = new_room
       draw_map(rooms) if draw
     end
 
+#    print "Remaining length #{remaining.length} depth #{depth}\n"
+    
     next_move, *remaining = remaining
   end
 
 end
 
-# Map of coordinates to rooms
-rooms = Hash.new{Room.new}
-start = [0,0]
-rooms[start] = Room.new
+expand_map(directions, [ROW_OFFSET,COL_OFFSET], Room.new, rooms, true, 0)
 
-expand_map(directions, [0,0], Room.new, rooms, true)
-
-draw_map(rooms)
+draw_map(rooms, true)
     
 # ┴ ULR
 # ┬ DLR
